dd931a320da81c63b0596209fc9fcfe3256d04a0
KevindeQ <k.dequillettes@gmail.com>
2024-01-13 20:42:19 +0100

    Fix shader compilation during build to actually work

--- cmake/ShaderSupport.cmake
+++ cmake/ShaderSupport.cmake
@@ -3,11 +3,11 @@ function(enable_build_shaders PARENT_TARGET)
 	add_dependencies(${PARENT_TARGET} shaders)
 endfunction()
 
-function(build_shaders SHADER_FILES SHADER_TYPE)
+function(build_shaders SHADER_FILES SHADER_TYPE OUTPUT_DIR)
 	foreach(FILE ${SHADER_FILES})
 		get_filename_component(FILE_WE ${FILE} NAME_WE)
 		add_custom_command(TARGET shaders
-			COMMAND dxc.exe /nologo /Emain /T${SHADER_TYPE} $<IF:$<CONFIG:DEBUG>,/Od,/O3> /Zi /Fo ${CMAKE_BINARY_DIR}/${FILE_WE}.cso /Fd ${CMAKE_BINARY_DIR}/${FILE_WE}.pdb ${FILE}
+			COMMAND dxc.exe /nologo /Emain /T${SHADER_TYPE} $<IF:$<CONFIG:DEBUG>,/Od,/O3> /Zi /Fo ${OUTPUT_DIR}/${FILE_WE}.cso /Fd ${OUTPUT_DIR}/${FILE_WE}.pdb ${FILE}
 		MAIN_DEPENDENCY ${FILE}
 		COMMENT "HLSL ${FILE}"
 		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
